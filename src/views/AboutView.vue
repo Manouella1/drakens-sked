ecc3b25667fc8dae59329e020d9087348fb1f8ec
majlen <maja.lennevi@iths.se>
2024-02-13 15:01:02 +0100

    Skapad json-fil med recept

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,10 @@
-<script></script>
+<script>
+// ============================================================
+// Exempel på hur man kan hämta data från meals.json-filen i mappen "assets".
+// Denna kod skriver ut "Pannkakor"
+import jsondata from '../assets/meals.json'
+console.log(jsondata.recipes[1].title)
+</script>
 
 <template>
   <h1>ABOUT</h1>
