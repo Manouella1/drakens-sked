be954b3946377cc6f9844fe3edc33087201edefd
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-15 14:12:06 +0100

    csss styles

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './style.css'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import './style.css'
 
 const app = createApp(App)
 
