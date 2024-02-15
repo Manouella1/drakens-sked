8fc96dd395069d05976f3ea9104af6819ccf4e43
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-15 13:20:52 +0100

    test to see if boostrap works

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,30 @@
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      value: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.value++
+    },
+  },
+}
+</script>
 
 <template>
   <h1>HOME</h1>
+  <BButton @click="increment" variant="primary">{{ value }}</BButton>
+
+  <BButton variant="primary">Primary</BButton>
+  <BButton variant="secondary">Secondary</BButton>
+  <BButton variant="success">Success</BButton>
+  <BButton variant="danger">Danger</BButton>
+  <BButton variant="warning">Warning</BButton>
+  <BButton variant="info">Info</BButton>
+  <BButton variant="light">Light</BButton>
+  <BButton variant="dark">Dark</BButton>
 </template>
 
 <style></style>
