13b0a96ddf7eab667fa94cac2574d1f2c49c92b5
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-15 13:25:43 +0100

    buttons boostrap tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,15 +16,6 @@ export default {
 <template>
   <h1>HOME</h1>
   <BButton @click="increment" variant="primary">{{ value }}</BButton>
-
-  <BButton variant="primary">Primary</BButton>
-  <BButton variant="secondary">Secondary</BButton>
-  <BButton variant="success">Success</BButton>
-  <BButton variant="danger">Danger</BButton>
-  <BButton variant="warning">Warning</BButton>
-  <BButton variant="info">Info</BButton>
-  <BButton variant="light">Light</BButton>
-  <BButton variant="dark">Dark</BButton>
 </template>
 
 <style></style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
