43cc5462bca10d1d14f69a14793cafffecd3e97a
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-16 11:38:10 +0100

    image not working

--- src/views/AllReceptsView.vue
+++ src/views/AllReceptsView.vue
@@ -2,7 +2,7 @@
 import meals from '../assets/meals.json'
 
 // console.log(meals.recipes[1].title)
-console.log(meals)
+console.log(meals.recipes[0].image)
 
 export default {
   created() {
@@ -19,8 +19,12 @@ export default {
 <template>
   <h1>ALL RECEPTS</h1>
   <ol>
-    <li v-for="meal in meals">{{ meal.title }}</li>
+    <li v-for="meal in meals">
+      <h2>{{ meal.title }}</h2>
+      <img :src="`/assets/${meal.image}`" alt="photo recept" />
+    </li>
   </ol>
+  <img src="../assets/bananpannkakor_ID_8.jpg" alt="photo recept" />
 </template>
 
 <style></style>
