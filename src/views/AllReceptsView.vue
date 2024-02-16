6faf2ab2e3bda189e7de7dd25bbbcc6307e874b2
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-16 13:31:24 +0100

    photo json not wornot working

--- src/views/AllReceptsView.vue
+++ src/views/AllReceptsView.vue
@@ -19,12 +19,16 @@ export default {
 <template>
   <h1>ALL RECEPTS</h1>
   <ol>
-    <li v-for="meal in meals">
+    <li v-for="meal in meals" :key="meal.id">
       <h2>{{ meal.title }}</h2>
-      <img :src="`/assets/${meal.image}`" alt="photo recept" />
+      <img :src="`../assets/${meal.image}`" alt="photo recept" />
     </li>
   </ol>
-  <img src="../assets/bananpannkakor_ID_8.jpg" alt="photo recept" />
+  <img src="../assets/foto1.jpg" alt="photo recept" />
+
+  <div>{{ meals[0].image }}</div>
+  <img :src="meals[0].image" alt="photo recept" />
+  <!-- <img :src="`../assets/${meals[0].image}`" alt="photo recept" /> -->
 </template>
 
 <style></style>
