de9d45099c3c106ae2031a9bd0e7750f2e3408e5
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-16 10:53:42 +0100

    json images test

--- src/views/AllReceptsView.vue
+++ src/views/AllReceptsView.vue
@@ -1,5 +1,20 @@
-<script></script>
+<script>
+import meals from '../assets/meals.json'
 
-<template><h1>ALL RECEPTS</h1></template>
+export default {
+  data() {
+    return {
+      meals: null,
+    }
+  },
+}
+</script>
+
+<template>
+  <h1>ALL RECEPTS</h1>
+  <ol>
+    <li v-for="meal in meals">{{ meal.title }}</li>
+  </ol>
+</template>
 
 <style></style>
