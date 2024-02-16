609bc8dcf6979fb0fda70edc95e5f8ff36d98d88
majlen <maja.lennevi@iths.se>
2024-02-16 13:59:42 +0100

    Ordnat dynamisk pratbubbla till draken på introsidan

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue'
+import ChatBubble from './ChatBubble.vue'
 </script>
 
 <template>
@@ -8,59 +9,22 @@ import { ref } from "vue";
     src="../assets/bilder/kitchenglovespng.png"
     alt=""
   />
-  <div class="bubble bubble-bottom-left" contenteditable>
-    Type any text here and the bubble will grow to fit the text no matter how
-    many lines.
-  </div>
+  <ChatBubble></ChatBubble>
   <img class="drake" src="../assets/bilder/drake-clear-kitchen.png" alt="" />
 </template>
 
 <style>
 .kitchengloves {
-  height: 400px;
+  max-width: 40vw;
 }
 .drake {
   border-image-repeat: no-repeat;
+  max-width: 100vw;
 }
 body {
   margin: 0;
   padding: 0;
   background-color: red;
   border: solid, red;
-}
-
-/* Chatt bubblan */
-
-.bubble {
-  position: relative;
-  font-family: sans-serif;
-  font-size: 18px;
-  line-height: 24px;
-  width: 300px;
-  background: #fff;
-  border-radius: 40px;
-  padding: 24px;
-  text-align: center;
-  color: #000;
-}
-
-.bubble-bottom-left:before {
-  content: "";
-  width: 0px;
-  height: 0px;
-  position: absolute;
-  right: 30px;
-  bottom: -24px;
-
-  /*the border expands from left to right designing a triangle */
-  border-left: 24px solid #fff;
-  /*the border expands from right to left designing a transparent triangle */
-  /*setting the angle of the pin */
-  border-right: 25px solid transparent;
-  /*the border expands from the top to the bottom designing a   triangle */
-  border-top: 12px solid #fff;
-  /*the border expands from the bottom to the top designing a transparent triangle */
-  /*setting the width of the pin */
-  border-bottom: 20px solid transparent;
 }
 </style>
