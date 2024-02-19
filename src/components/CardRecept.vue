c5fe297f985ac049ca1221c7fa5297ab1208207b
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-19 11:51:07 +0100

    create commponents card ans cards

---
+++ src/components/CardRecept.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+  <div>
+    <img />
+    <h2>Title</h2>
+    <p>time</p>
+    <p>Kategorier</p>
+    <p>Icon</p>
+    <p>Nivå</p>
+  </div>
+</template>
+
+<style></style>
