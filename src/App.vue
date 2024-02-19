cbe80202a5f66fca60189ac8db3cdbf4f854ba05
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-19 22:43:46 +0100

    ReceptView updated and comments APP.vue for Footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <script>
-import NavBarMenu from "./components/NavBar.vue";
-import ScrollToTopButton from "./components/ScrollToTopButton.vue"; // kan tas bort
-import Footer from "./components/Footer.vue";
+import NavBarMenu from './components/NavBar.vue'
+import ScrollToTopButton from './components/ScrollToTopButton.vue' // kan tas bort
+import Footer from './components/Footer.vue'
 
 export default {
   components: {
     NavBarMenu,
     ScrollToTopButton, //kan tas bort när man inte vill ha knapp på alla sidor
+    Footer,
   },
-};
+}
 </script>
 <template>
   <NavBarMenu />
-  <ScrollToTopButton /> <!-- kan tas bort -->
-   <!-- <v-footer>
+  <ScrollToTopButton />
+  <!-- kan tas bort -->
+  <!-- <v-footer>
     <Footer />
   </v-footer> -->
   <!-- Denna utkommenterade tillhör footern med länkar som inte fungerar -->
+
+  <!-- Footer is after the router view as this one print the content of the pages -->
+  <!-- but if we moved after is not showing!! ?? CHECK THIS! -->
   <Footer />
+
+  <!-- Here print the content of the pages on the DOM -the routes -->
   <router-view />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
