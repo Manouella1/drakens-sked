7b0bc0f1a061a6f876f0cb84de07a7d2b35d937d
YuvanVH <vanessa-huynh@hotmail.com>
2024-02-19 15:45:16 +0100

    scroll knapp

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 import NavBarMenu from "./components/NavBar.vue";
+import ScrollToTopButton from "./components/ScrollToTopButton.vue"; // kan tas bort
+
 export default {
   components: {
     NavBarMenu,
+    ScrollToTopButton, //kan tas bort när man inte vill ha knapp på alla sidor
   },
 };
 </script>
-
 <template>
   <NavBarMenu />
+  <ScrollToTopButton /> <!-- kan tas bort -->
   <router-view />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
