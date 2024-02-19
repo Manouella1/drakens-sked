2810adbce4caec79aef55d1386f18376248c6763
YuvanVH <vanessa-huynh@hotmail.com>
2024-02-19 15:49:58 +0100

    scroll knapp

---
+++ src/components/ScrollToTopButton.vue
@@ -0,0 +1,63 @@
+<template>
+  <button v-if="showButton" @click="scrollToTop" class="scroll-to-top-button">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+/*Scroll funktion */
+export default {
+  data() {
+    return {
+      showButton: false,
+      buttonText: "^"
+    };
+  },
+  // Lägger till en lyssnare för att hantera scrollhändelser
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  // Tar bort scrollhändelse-lyssnare
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  // Funktionen som körs när användaren scrollar
+  methods: {
+    handleScroll() {
+      this.showButton = window.scrollY > 100;
+    },
+    // Funktion för att scrolla upp till toppen
+    scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"  // Gör scrollningen smooooth~
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.scroll-to-top-button {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  z-index: 999;
+  background-color: #1c994a46;
+  color: #fff;
+  border: none;
+  border-radius: 30px;
+  padding: 5px 12px;
+  cursor: pointer;
+}
+
+/* Efter hover */
+.scroll-to-top-button:hover {
+  background-color: rgba(24, 160, 67, 0.976);
+  padding: 20px 20px;
+}
+
+.scroll-to-top-button:hover::before {
+  content: "Scrolla till toppen";
+}
+</style>
