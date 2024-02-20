50c5bf5a7b32e15c08f7ae9b3e903f9b5ec41247
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-20 10:39:12 +0100

    contact view exempel import Recepts

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,10 @@
-<script></script>
+<script>
+import CardsRecepts from '../components/CardsRecepts.vue'
+</script>
 
 <template>
   <h1>Contact</h1>
+  <CardsRecepts />
 </template>
 
 <style></style>
