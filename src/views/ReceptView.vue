ae70d942a7d0d47987f629db8dc4671d2dd51b47
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-20 10:05:30 +0100

    ReceptView all data is showing

--- src/views/ReceptView.vue
+++ src/views/ReceptView.vue
@@ -13,41 +13,68 @@ export default {
 
     this.selectedMeal = meals.recipes.find((meal) => meal.id === selectedMealId)
     console.log(this.selectedMeal)
+
+    this.ingredients = this.selectedMeal.ingredients
+    this.instructions = this.selectedMeal.instructions
+    this.nutrients = this.selectedMeal.nutrients
   },
   data() {
     return {
       selectedReceptId: '',
       selectedMeal: {},
+      ingredients: null,
+      instructions: null,
+      nutrients: null,
     }
   },
 }
 </script>
 
 <template>
-  <h1>RECEPT</h1>
-  some icon?
-  {{ selectedMeal.level }}
-  {{ selectedMeal.category }}
-  {{ selectedMeal.prepTime }}
-  {{ selectedMeal.cookTime }}
-  {{ selectedMeal.nutrients }}
-  {{ selectedMeal.portions }}
-  <p>recept ID: {{ $route.params.receptId }}</p>
-  <img :src="`src/assets/${selectedMeal.image}`" />
-  <h2>{{ selectedMeal.title }}</h2>
-  <p>{{ selectedMeal.description }}</p>
+  <main class="container">
+    <h1>RECEPT</h1>
 
-  <h3>Ingredienser:</h3>
-  <ol></ol>
-  <p>{{ selectedMeal.ingredients }}</p>
+    <div class="flex">
+      some icon?
+      <p>{{ selectedMeal.level }}</p>
+      <p>{{ selectedMeal.category }}</p>
+      <p>
+        PrepTime:{{ selectedMeal.prepTime }} CookTime:{{
+          selectedMeal.cookTime
+        }}
+      </p>
+      Nutrients:
+      <ul>
+        <li v-for="nutrient in nutrients">{{ nutrient }}</li>
+      </ul>
+      <p>Portioner:{{ selectedMeal.portions }}</p>
+      <!-- Nutrients:{{ selectedMeal.nutrients }}  -->
+    </div>
+    <p>recept ID: {{ $route.params.receptId }}</p>
+    <img :src="`src/assets/${selectedMeal.image}`" />
+    <h2>{{ selectedMeal.title }}</h2>
+    <p>{{ selectedMeal.description }}</p>
 
-  <h3>Gör så här:</h3>
+    <h3>Ingredienser:</h3>
+    <ul>
+      <li v-for="ingredient in ingredients">{{ ingredient }}</li>
+    </ul>
 
-  <p>{{ selectedMeal.instructions }}</p>
+    <h3>Gör så här:</h3>
+
+    <ol>
+      <li v-for="instruction in instructions">{{ instruction }}</li>
+    </ol>
+  </main>
 </template>
 
 <style scoped>
 img {
   max-height: 200px;
+}
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 </style>
