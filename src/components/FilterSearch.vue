f774f91781f8fbf33de1d3f4cbd56b560fc2b92a
majlen <maja.lennevi@iths.se>
2024-02-20 13:46:13 +0100

    Skapad komponent 'FilterSearch' och gjort funktion som genererar vald svårighetsgrad

---
+++ src/components/FilterSearch.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+import { BButton } from 'bootstrap-vue-next'
+import meals from '../assets/meals.json'
+
+let allLevelRecipes = ref(null)
+
+function levelRecipe(x) {
+  allLevelRecipes.value = []
+  for (let i = 0; i < meals.recipes.length; i++) {
+    if (x === meals.recipes[i].level) {
+      allLevelRecipes.value.push(meals.recipes[i].id)
+    }
+  }
+  console.log(`${allLevelRecipes.value}`)
+}
+</script>
+
+<template>
+  <div>
+    <BButton @click="levelRecipe('Basic')" pill variant="outline-secondary"
+      >Lätt</BButton
+    >
+    <BButton
+      @click="levelRecipe('Intermedium')"
+      pill
+      variant="outline-secondary"
+      >Mellan</BButton
+    >
+    <BButton @click="levelRecipe('Advanced')" pill variant="outline-secondary"
+      >Avancerad</BButton
+    >
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: white;
+}
+</style>
