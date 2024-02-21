cbc952668842ebe1dc9dea47a09629571197c3c5
YuvanVH <vanessa-huynh@hotmail.com>
2024-02-21 08:15:48 +0100

    Uppgraderat MyFavoriteReceptsView.vue

--- src/views/MyFavoriteReceptsView.vue
+++ src/views/MyFavoriteReceptsView.vue
@@ -1,19 +1,27 @@
-<template>
-  <div>
-    <h1>Favorit Recipes</h1>
-    <favorite-recipes></favorite-recipes>
-  </div>
-</template>
-
 <script>
 import FavoriteRecipes from '../components/FavoriteRecepts.vue'; // Importera komponenten
 
 export default {
   components: {
-    FavoriteRecipes, // Lägg till komponenten här
+    FavoriteRecipes,
+  },
+  methods: {
+    addToFavorites(recipeId) {
+      // Hämta nuvarande favoritrecept från localStorage
+      let favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+
+      // Kontrollera om receptet redan finns i favoriter/mina sidor
+      if (!favoriteRecipes.includes(recipeId)) {
+        // Om receptet inte redan finns, lägg till det i favoriter
+        favoriteRecipes.push(recipeId);
+
+        // Spara uppdaterade favoritrecept i localStoragee
+        localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes));
+      } else {
+        // Om receptet redan finns i favoriter, ge användaren en varning elr gör ingenting
+        alert('Receptet finns redan i dina favoriter!');
+      }
+    },
   },
 };
 </script>
-<style>
-/* Lägger till still här! */
-</style>
