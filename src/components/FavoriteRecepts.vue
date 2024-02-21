b617a9263af457ce4e3f651377123b29c1003bec
YuvanVH <vanessa-huynh@hotmail.com>
2024-02-21 12:41:18 +0100

    Added message to heart, localStorage not done

--- src/components/FavoriteRecepts.vue
+++ src/components/FavoriteRecepts.vue
@@ -1,12 +1,13 @@
+<!-- src/components/FavoriteRecepts.vue -->
 <template>
   <div>
     <!-- Visa en lista överr favoritrecept -->
     <ul>
       <!-- Loopa genom favoritrecepten -->
-      <li v-for="recipe in favoriteRecipes" :key="recipe.id">
+      <li v-for="recipe in favoriteRecipes" :key="recipe.Id">
         {{ recipe.title }}
         <!-- Skapa en knapp för att ta bort receptet från favoriter -->
-        <button @click="removeFromFavorites(recipe.id)"> Remove </button>
+        <button @click="removeFromFavorites(recipe.Id)"> Remove </button>
       </li>
     </ul>
   </div>
@@ -37,7 +38,7 @@ export default {
     // Metod för att ta bort ett recept från favoriter
     removeFromFavorites(recipeId) {
       // Filtrera ut receptet med angivet ID från favoritrecepten
-      this.favoriteRecipes = this.favoriteRecipes.filter(recipe => recipe.id !== recipeId);
+      this.favoriteRecipes = this.favoriteRecipes.filter(recipe => recipe.Id !== recipeId);
       // Uppdatera lokal lagring med nya favoritrecept efter borttagning
       localStorage.setItem('favoriteRecipes', JSON.stringify(this.favoriteRecipes));
     },
