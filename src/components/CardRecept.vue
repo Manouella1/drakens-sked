e837838c85f96b30bafdb15b837c8193cbc88f08
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-22 14:41:35 +0100

    colors replace icon

--- src/components/CardRecept.vue
+++ src/components/CardRecept.vue
@@ -1,8 +1,17 @@
 <!-- src/components/CardRecept.vue -->
 <template>
   <!-- Receiving props from parent component -->
-  <BCard :title="title" :img-src="`src/assets/${image}`" img-alt="Image" img-top tag="article" style="max-width: 17rem"
-    @mouseover="enlargeImage" @mouseout="resetImage" @click="handleClick">
+  <BCard
+    :title="title"
+    :img-src="`src/assets/${image}`"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 17rem"
+    @mouseover="enlargeImage"
+    @mouseout="resetImage"
+    @click="handleClick"
+  >
     <BCardText>
       <div class="flex">
         <!-- time -->
@@ -10,74 +19,83 @@
         <!-- Kategorier  -->
         <p>- {{ category }} -</p>
         <!-- we still need to add iton the json file -->
-        <p>Icon</p>
+        <p>{{ icon }}</p>
         <!-- Nivå  -->
         <p>{{ level }}</p>
       </div>
     </BCardText>
 
     <!-- Lägg till knapp för att lägga till i favoriter -->
-    <BButton pill @click.stop="addToFavorites" variant="outline-secondary">❤️</BButton>
-    <span v-if="showConfirmation" class="confirmation">{{ confirmationMessage }}</span>
+    <BButton pill @click.stop="addToFavorites" variant="outline-secondary"
+      >❤️</BButton
+    >
+    <span v-if="showConfirmation" class="confirmation">{{
+      confirmationMessage
+    }}</span>
   </BCard>
 </template>
 
 <script>
-export default {
-  props: ['title', 'prepTime', 'category', 'level', 'image', 'id'],
-  data() {
-    return {
-      showConfirmation: false,
-      isFavorite: false, // Boolean för att spåra om receptet är en favorit
-      confirmationMessage: '', // Meddelande för bekräftelse
-    };
-  },
-  methods: {
-    enlargeImage(event) {
-      // Enlarge the image when hovered
-      event.currentTarget.style.transform = 'scale(1.2)'
-      event.currentTarget.style.cursor = 'pointer'
+  export default {
+    props: ['title', 'prepTime', 'category', 'level', 'image', 'id', 'icon'],
+    data() {
+      return {
+        showConfirmation: false,
+        isFavorite: false, // Boolean för att spåra om receptet är en favorit
+        confirmationMessage: '' // Meddelande för bekräftelse
+      }
     },
+    methods: {
+      enlargeImage(event) {
+        // Enlarge the image when hovered
+        event.currentTarget.style.transform = 'scale(1.2)'
+        event.currentTarget.style.cursor = 'pointer'
+      },
 
-    resetImage(event) {
-      // Reset the image when mouseout
-      event.currentTarget.style.transform = 'scale(1)'
-      event.currentTarget.style.cursor = 'default'
-    },
-    handleClick() {
-      // Emit the 'selectRecept' event with the meal ID
-      this.$emit('selectRecept', this.id)
-    },
-    addToFavorites() {
-      // Toggle isFavorite status
-      this.isFavorite = !this.isFavorite;
+      resetImage(event) {
+        // Reset the image when mouseout
+        event.currentTarget.style.transform = 'scale(1)'
+        event.currentTarget.style.cursor = 'default'
+      },
+      handleClick() {
+        // Emit the 'selectRecept' event with the meal ID
+        this.$emit('selectRecept', this.id)
+      },
+      addToFavorites() {
+        // Toggle isFavorite status
+        this.isFavorite = !this.isFavorite
 
-      // Uppdatera bekräftelsemeddelande beroende på isFavorite-status
-      this.confirmationMessage = this.isFavorite ? 'Receptet har lagts till i dina favoriter!' : 'Receptet har tagits bort från dina favoriter!';
+        // Uppdatera bekräftelsemeddelande beroende på isFavorite-status
+        this.confirmationMessage = this.isFavorite
+          ? 'Receptet har lagts till i dina favoriter!'
+          : 'Receptet har tagits bort från dina favoriter!'
 
-      // Emit the 'toggleFavorite' event with the recipe ID and its status
-      this.$emit('toggleFavorite', { id: this.id, isFavorite: this.isFavorite });
+        // Emit the 'toggleFavorite' event with the recipe ID and its status
+        this.$emit('toggleFavorite', {
+          id: this.id,
+          isFavorite: this.isFavorite
+        })
 
-      // Visa bekräftelsemeddelandet
-      this.showConfirmation = true;
+        // Visa bekräftelsemeddelandet
+        this.showConfirmation = true
 
-      // Dölj bekräftelsemeddelandet efter 3 sekunder
-      setTimeout(() => {
-        this.showConfirmation = false;
-      }, 3000);
-    },
-  },
-};
+        // Dölj bekräftelsemeddelandet efter 3 sekunder
+        setTimeout(() => {
+          this.showConfirmation = false
+        }, 3000)
+      }
+    }
+  }
 </script>
 
 <style scoped>
-.flex {
-  display: flex;
-  justify-content: space-between;
-}
+  .flex {
+    display: flex;
+    justify-content: space-between;
+  }
 
-.confirmation {
-  color: green;
-  font-size: 14px;
-}
+  .confirmation {
+    color: green;
+    font-size: 14px;
+  }
 </style>
--- src/components/CardsRecepts.vue
+++ src/components/CardsRecepts.vue
@@ -1,31 +1,31 @@
 <script>
-import meals from '../assets/meals.json'
-import CardRecept from '../components/CardRecept.vue'
+  import meals from '../assets/meals.json'
+  import CardRecept from '../components/CardRecept.vue'
 
-export default {
-  components: {
-    CardRecept,
-  },
-  created() {
-    this.meals = meals.recipes
-    // Assigning meals data to meals array
-  },
-  data() {
-    return {
-      meals: [],
-      // Initializing meals array
-    }
-  },
-  methods: {
-    selectRecept(mealId) {
-      // Emit an event with the selected meal ID
-      // this.$emit('selectRecept', meal)
-      console.log('Selected Meal ID:', mealId)
-      // Navigate to ReceptView with the selected mealId
-      this.$router.push({ name: 'Recept', params: { receptId: mealId } })
+  export default {
+    components: {
+      CardRecept
     },
-  },
-}
+    created() {
+      this.meals = meals.recipes
+      // Assigning meals data to meals array
+    },
+    data() {
+      return {
+        meals: []
+        // Initializing meals array
+      }
+    },
+    methods: {
+      selectRecept(mealId) {
+        // Emit an event with the selected meal ID
+        // this.$emit('selectRecept', meal)
+        console.log('Selected Meal ID:', mealId)
+        // Navigate to ReceptView with the selected mealId
+        this.$router.push({ name: 'Recept', params: { receptId: mealId } })
+      }
+    }
+  }
 </script>
 
 <template>
@@ -42,6 +42,7 @@ export default {
         :level="meal.level"
         :image="meal.image"
         :id="meal.id"
+        :icon="meal.icon"
         @selectRecept="selectRecept"
       >
         <!-- THIS WAS INSIDE THE CARD RECEPT CAUSING ERROR IN TERMINAL: IS NOT DEFINED ON INSTANCE -->
