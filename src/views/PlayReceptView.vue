250c1d44e42d2d1b12cafe259ff6be0a2678b014
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-02-22 12:54:18 +0100

    error emit resolved

--- src/views/PlayReceptView.vue
+++ src/views/PlayReceptView.vue
@@ -1,40 +1,40 @@
 <script>
-export default {
-  props: {
-    instructions: {
-      type: Array,
-      required: true,
+  export default {
+    props: {
+      instructions: {
+        type: Array,
+        required: true
+      }
     },
-  },
-  // emits
-  data() {
-    return {
-      receptId: '1', // Hardcoded value for testing
-      step: 'step1', // Hardcoded value for testing
-    }
-  },
-  beforeRouteLeave(to, from, next) {
-    this.receptId = to.params.receptId
-    this.step = to.params.step
-    console.log('beforeRouteUpdate', this.receptId, this.step)
-    next()
-  },
+    emits: ['handlePlayReceptBack'],
+    data() {
+      return {
+        receptId: '1', // Hardcoded value for testing
+        step: 'step1' // Hardcoded value for testing
+      }
+    },
+    beforeRouteLeave(to, from, next) {
+      this.receptId = to.params.receptId
+      this.step = to.params.step
+      console.log('beforeRouteUpdate', this.receptId, this.step)
+      next()
+    },
 
-  // just to check if works
-  // created() {
-  //   console.log('created', this.receptId, this.step)
-  // },
-  mounted() {
-    console.log('mounted', this.receptId, this.step) // for testing
-  },
-  methods: {
-    goBack() {
-      this.$router.go(-1) // uses the router to go back
-      // this.$emit('handlePlayReceptBack', (this.showReceptView = true))
-      this.$emit('handlePlayReceptBack') // Not working
+    // just to check if works
+    // created() {
+    //   console.log('created', this.receptId, this.step)
+    // },
+    mounted() {
+      console.log('mounted', this.receptId, this.step) // for testing
     },
-  },
-}
+    methods: {
+      goBack() {
+        this.$router.go(-1) // uses the router to go back
+        // this.$emit('handlePlayReceptBack', (this.showReceptView = true))
+        this.$emit('handlePlayReceptBack') // Not working
+      }
+    }
+  }
 </script>
 
 <template>
