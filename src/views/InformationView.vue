57c2a76c96dadfe921ff1ccbfc9239d1d6056a8b
Vigan Kumnova <vigan.kumnova@gmail.com>
2024-02-22 14:16:42 +0100

    rättade till menyn

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -15,12 +15,11 @@
   <div class="advancedmeal">
     <h2>Avancerat</h2>
     <p>Avancerade recept kräver lite mer erfarenhet och kunskap i köket. Du behöver en vuxen som kan assistera! Dessa recept kan innehålla mer avancerade tekniker, längre tillagningstid eller mer avancerade ingredienser. Perfekt för dig som vill tillaga något godare men även ha kul tillsammans med någon annan äldre under tiden.
-      en kommentar bara för att se om vanja kan hjälpa mig och Maja. Test iggen
     </p>
   </div>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 .basicmeal {
 
 }
@@ -30,4 +29,4 @@
 .advacedmeal {
 
 }
-</style>
+</style> -->
