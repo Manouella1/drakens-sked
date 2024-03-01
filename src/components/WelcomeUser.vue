6c18c3b30abf8164271ec9156e5b97b46a165b22
Maja Lennevi <majlen30@gmail.com>
2024-03-01 10:32:52 +0100

    Uppgradering av drakens fraser

--- src/components/WelcomeUser.vue
+++ src/components/WelcomeUser.vue
@@ -1,23 +1,52 @@
 <script setup>
   import { ref } from 'vue'
 
-  const name = ref(null) // användarens namn
+  const name = ref(null), // användarens namn
+    phrasesStart = ref([
+      'Vad ska vi laga för smarrigt idag, ',
+      '',
+      'Visste du ',
+      '',
+      'Vad är du sugen på idag, ',
+      'Hur mår du idag, ',
+      'Hejsan hoppsan '
+    ]), // Början på fraser
+    phrasesEnd = ref([
+      '?',
+      ', visste du att drakar har bra luktsinne?',
+      ', att drakar älskar att laga mat?',
+      ', när jag är hungrig lyser min mage som eld',
+      '?',
+      '?',
+      '!'
+    ]), // Slutet på fraser
+    randomPhraseStart = ref(null), // Slumpad startfras
+    randomPhraseEnd = ref(null) // Slumpad slutfras
 
   // hämtar användarens namn
   name.value = localStorage.getItem('name')
   if (name.value === null) {
     name.value = 'kompis'
   }
+
+  // Genererar slumpad fras
+  let x = Math.floor(Math.random() * phrasesStart.value.length)
+  randomPhraseStart.value = phrasesStart.value[x]
+  randomPhraseEnd.value = phrasesEnd.value[x]
 </script>
 
 <template>
   <div>
     <h1>
-      Vad ska vi laga för smarrigt idag,
-      <span style="color: #219c56">{{ name }}</span
-      >?
+      {{ randomPhraseStart }}
+      <span style="color: #219c56">{{ name }}</span>
+      {{ randomPhraseEnd }}
     </h1>
-    <img class="drake" src="../assets/bilder/drake-vinkar-halv.png" alt="" />
+    <img
+      class="drake"
+      src="../assets/bilder/drake-vinkar-halv.png"
+      alt="Bild på drake"
+    />
   </div>
 </template>
 
