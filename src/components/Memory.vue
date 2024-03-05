fdb88ede89fb86cb500f6a6af111d84e6a12bf8e
Vigan Kumnova <vigan.kumnova@gmail.com>
2024-03-05 13:48:39 +0100

    memory game

--- src/components/Memory.vue
+++ src/components/Memory.vue
@@ -1,196 +1,174 @@
-<!-- Vigan Logic här -->
-
 <script>
-  export default {
-    name: 'MemoryGame',
-    data() {
-      return {
-        boxData: [
-          { id: 1, value: '🐶', show: false },
-          { id: 2, value: '🐱', show: false },
-          { id: 3, value: '🐵', show: false },
-          { id: 4, value: '🐰', show: false },
-          { id: 5, value: '🦄', show: false },
-          { id: 6, value: '🦂', show: false },
-          { id: 7, value: '⚽️', show: false },
-          { id: 8, value: '🏀', show: false },
-          { id: 9, value: '🏐', show: false },
-          { id: 10, value: '🐰', show: false },
-          { id: 11, value: '🐵', show: false },
-          { id: 12, value: '🏈', show: false },
-          { id: 13, value: '🏀', show: false },
-          { id: 14, value: '⚽️', show: false },
-          { id: 15, value: '🐶', show: false },
-          { id: 16, value: '🦄', show: false },
-          { id: 15, value: '🦂', show: false },
-          { id: 15, value: '🏐', show: false },
-          { id: 15, value: '🐱', show: false },
-          { id: 15, value: '🏈', show: false }
-        ],
-        currVal: null,
-        result: ''
+import MemoryCard from './MemoryCard.vue'
+
+export default {
+  name: 'MemoryGame',
+  components: {
+    MemoryCard
+  },
+  data() {
+    return {
+      cards: [],
+      flippedCards: [],
+      totalTry: 0,
+      mainCards: [
+        { id: 1, value: '🎠', matched: false, flipped: false },
+        { id: 2, value: '🎠', matched: false, flipped: false },
+        { id: 3, value: '🦁', matched: false, flipped: false },
+        { id: 4, value: '🦁', matched: false, flipped: false },
+        { id: 5, value: '🍓', matched: false, flipped: false },
+        { id: 6, value: '🍓', matched: false, flipped: false },
+        { id: 7, value: '🎈', matched: false, flipped: false },
+        { id: 8, value: '🎈', matched: false, flipped: false },
+        { id: 9, value: '🐶', matched: false, flipped: false },
+        { id: 10, value: '🐶', matched: false, flipped: false },
+        { id: 11, value: '🍌', matched: false, flipped: false },
+        { id: 12, value: '🍌', matched: false, flipped: false },
+        { id: 13, value: '🍎', matched: false, flipped: false },
+        { id: 14, value: '🍎', matched: false, flipped: false },
+        { id: 15, value: '🍉', matched: false, flipped: false },
+        { id: 16, value: '🍉', matched: false, flipped: false }
+        // Lägga till fler rader om vi vill fylla sidan mer.
+      ]
+    }
+  },
+
+  methods: {
+    shuffleCards() {
+      this.cards = [...this.mainCards] // Starta om kort till mainCards
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
       }
     },
-    methods: {
-      showBox(_id, _value) {
-        this.currVal = _value
-        console.log('Values:', this.currVal, _value)
+    flipCard(index) {
+      if (this.flippedCards.length < 2 && !this.cards[index].flipped) {
+        this.cards[index].flipped = true
+        this.flippedCards.push(index)
 
-        this.boxData[_id].show = true
-        if (this.currVal === null) {
-          this.currVal = _value
-        } else if (_value === this.currVal) {
-          this.result = 'Great!'
-          this.currVal = null
-        } else {
-          this.result = 'Försök igen!'
-          this.boxData.map((box) => (box.show = false))
-          this.currVal = null
+        if (this.flippedCards.length === 2) {
+          this.checkMatch()
         }
       }
+    },
+    checkMatch() {
+      const [index1, index2] = this.flippedCards
+      if (this.cards[index1].value === this.cards[index2].value) {
+        setTimeout(() => {
+          this.cards[index1].matched = true
+          this.cards[index2].matched = true
+        }, 500)
+      } else {
+        setTimeout(() => {
+          this.cards[index1].flipped = false
+          this.cards[index2].flipped = false
+        }, 500)
+      }
+      this.totalTry++
+
+      this.flippedCards = []
+    },
+    restartGame() {
+      this.cards.forEach((card) => {
+        card.flipped = false
+        card.matched = false
+      })
+
+      this.flippedCards = []
+      this.shuffleCards()
+      this.totalTry = 0
+    }
+  },
+
+  mounted() {
+    this.shuffleCards()
+  },
+  computed: {
+    isGameComplete() {
+      return (
+        this.mainCards.length ===
+        this.mainCards.filter((card) => card.matched).length
+      )
     }
   }
+}
 </script>
 
+
 <template>
-  <h1>Memory</h1>
-  <div class="gameArea">
-    <!-- kanske döper denna till memory istället -->
-    <h4>Senast valda kort: {{ currVal }}</h4>
-    <div class="boxHolder" />
-    <div class="box" v-for="(box, index) in boxData" :key="index">
-      <button
-        :class="box.show ? 'show' : ''"
-        @click="showBox(index, box.value)"
-      >
-        <p v-if="!box.show">Memory Kort</p>
-        <span>{{ box.value }}</span>
-      </button>
+  <div class="memory-game">
+    <div class="try">Antal försök : {{ totalTry }}</div>
+    <div v-if="isGameComplete" class="game-complete-message">Bra spelat</div>
+    <div class="cards-grid">
+      <MemoryCard
+        v-for="(card, index) in cards"
+        :key="index"
+        :card="card"
+        @click="flipCard(index)"
+      />
     </div>
+
+    <button @click="restartGame" class="style-btn">Starta om</button>
   </div>
 </template>
+
 
 <style scoped>
-  * {
-    transition: all 0.5s;
-  }
+.memory-game {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-  .gameArea {
-    width: 100%;
-    float: left;
-  }
-  .boxHolder {
-    display: block;
-    width: 800px;
-    margin: 0 auto;
-  }
-  .box {
-    width: 200px;
-    float: left;
-  }
-  button p {
-    margin-top: 45px;
-    margin-bottom: 0;
-  }
-  button.show p {
-    margin: 0;
-  }
-  .box button {
-    width: 100%;
-    height: 100px;
-    border: 2px solid #ddd;
-    border-radius: 0;
-  }
-  .box button span {
-    font-size: 50px;
-    opacity: 0;
-  }
+.cards-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 0fr);
+  gap: 10px;
+  place-items: center;
+}
+
+.card {
+  width: 100px;
+  height: 100px;
+  background-color: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 2em;
+}
+
+.flipped {
+  transform: rotateY(180deg);
+}
+
+/* .matched {
+  visibility: hidden;
+} */
+
+.game-complete-message {
+  text-align: center;
+  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 1.5em;
+  color: green;
+}
+
+.style-btn {
+  border-radius: 5px;
+  padding: 6px 10px;
+  border: none;
+  background-color: indianred;
+  color: white;
+  margin-top: 20px;
+}
+
+.try {
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 5px;
+  margin: 20px 0px;
+}
 </style>
-
-<!-- <script setup>
-  // Här presenterar vi våra spel
-</script>
-
-<template>
-  <div />
-  <div class="bubble bubble-bottom-left">
-    <h1 id="Text">Vilket spel ska vi spela?</h1>
-    <div>
-      <button class="bubble-gum">Memory</button>
-      <button class="bubble-gum">Quiz</button>
-    </div>
-  </div>
-  <img src="../assets/bilder/drake-visar.png" alt="" />
-</template>
-
-<style scoped lang="scss">
-  @import url('https://fonts.googleapis.com/css?family=Dongle');
-
-  #Text {
-    color: #219c56;
-  }
-
-  div {
-    margin-bottom: 30px;
-  }
-
-  /* Knapp styles*/
-  .bubble-gum {
-    margin-right: 2rem;
-    // margin-left: 40px;
-    margin-top: 20px;
-    padding: 2.2rem;
-    background: linear-gradient(to bottom right, hotpink, deeppink);
-    font-family: dongle;
-    font-size: 5rem;
-    color: white;
-    text-shadow: 0 0 8px indigo;
-    transition: transform 1s, border-radius 1s;
-    border: none;
-    border-radius: 20px;
-  }
-  .bubble-gum:hover {
-    cursor: pointer;
-    border: 2px solid indigo;
-    box-shadow: 0 0 8px aqua;
-    background: linear-gradient(to right, hotpink, deeppink);
-    transform: scale(1.4, 1.4);
-    border-radius: 50%;
-  }
-
-  /* Pratbubblan */
-  .bubble {
-    margin-top: 10px;
-    position: relative;
-    font-family: sans-serif;
-    font-size: 18px;
-    line-height: 24px;
-    margin: auto;
-    max-width: 100vw;
-    background: #fff;
-    border-radius: 40px;
-    padding: 50px;
-    text-align: center;
-    color: #000;
-  }
-
-  .bubble-bottom-left:before {
-    content: '';
-    width: 0px;
-    height: 0px;
-    position: absolute;
-    right: 30px;
-    bottom: -24px;
-
-    /*the border expands from left to right designing a triangle */
-    border-left: 24px solid #fff;
-    /*the border expands from right to left designing a transparent triangle */
-    /*setting the angle of the pin */
-    border-right: 25px solid transparent;
-    /*the border expands from the top to the bottom designing a   triangle */
-    border-top: 12px solid #fff;
-    /*the border expands from the bottom to the top designing a transparent triangle */
-    /*setting the width of the pin */
-    border-bottom: 20px solid transparent;
-  }
-</style> -->
---
+++ src/components/MemoryCard.vue
@@ -0,0 +1,106 @@
+<script>
+export default {
+  name: 'MemoryCard',
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div
+    class="card"
+    :class="{ flipped: card.flipped, matched: card.matched }"
+    @click="handleClick"
+  >
+    <div class="card-inner">
+      <div class="card-front"></div>
+      <div class="card-back">{{ card.value }}</div>
+      <div v-if="card.matched" class="match-text">Match</div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'MemoryCard',
+  props: {
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  width: 100px;
+  height: 100px;
+  perspective: 1000px;
+  position: relative;
+}
+
+.card-inner {
+  width: 100%;
+  height: 100%;
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+  position: relative;
+}
+
+.card-front,
+.card-back {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+}
+
+.card-front {
+  background-color: lightblue;
+}
+
+.card-back {
+  transform: rotateY(180deg);
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+}
+
+.flipped .card-inner {
+  transform: rotateY(180deg);
+}
+
+/* .matched .card-inner {
+  visibility: hidden;
+} */
+
+.match-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1em;
+  color: green;
+  font-weight: bold;
+  display: block; /* Säkrar så att "Matching text" visas */
+}
+</style>
