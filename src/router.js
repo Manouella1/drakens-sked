17af3622cbd721ec919dc077168dca800206423b
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-03-05 22:46:08 +0100

    update routes

--- src/router.js
+++ src/router.js
@@ -13,7 +13,7 @@ import MyFavoriteReceptsView from './views/MyFavoriteReceptsView.vue'
 import QuizView from './views/QuizView.vue'
 import IntroQuizView from './views/IntroQuizView.vue'
 
-import PlayDemoView from './views/PlayDemoView.vue'
+// import PlayDemoView from './views/PlayDemoView.vue'
 import MemoryGameView from './views/MemoryGameView.vue'
 
 export default createRouter({
@@ -24,14 +24,6 @@ export default createRouter({
       path: '/games/memory',
       name: 'Memory'
     },
-<<<<<<< HEAD
-=======
-    // {
-    //   component: PlayDemoView,
-    //   path: '/recepts/:id/:step',
-    //   name: 'Demo'
-    // },
->>>>>>> f08926435762288a878cadc899a34e63151f1d7f
 
     // {
     //   component: PlayDemoView,
