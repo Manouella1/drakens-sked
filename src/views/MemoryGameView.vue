8efcbd38e46ff9c31d82262516c7233e06678513
Vigan Kumnova <vigan.kumnova@gmail.com>
2024-03-06 15:28:24 +0100

    lite css memory

--- src/views/MemoryGameView.vue
+++ src/views/MemoryGameView.vue
@@ -5,9 +5,56 @@ export default {
   components: {
     MemoryGame
   }
-}</script>
+}
+</script>
 
 <template>
-<h1>Game</h1>
-<MemoryGame></MemoryGame>
+<div class="container">
+<h1>Memory Spel</h1>
+<p>Spela Memory där du ska hitta 2 likadana kort för att få en matchning.<br>Hitta 2 likadana kort och försök slå ditt rekord med färre klick.</p>
+</div>
+<div class="container2">
+<MemoryGame />
+</div>
 </template>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    font-size: 50px;
+  }
+  p {
+    color: white;
+    text-align: center;
+  }
+  .container {
+  display: inline-block;
+  margin-top: 30px;
+  font-size: 22px;
+  background-color: #219c56ee;
+  color: white;
+  border-radius: 50px;
+  padding: 20px;
+  max-width: 700px;
+}
+.container2 {
+  margin-bottom: 0px;
+}
+@media (max-width: 600px) {
+    .container {
+      font-size: 1em;
+      font-weight: normal;
+    }
+
+    .filter {
+      flex-direction: column;
+      margin: 0;
+    }
+  }
+  @media (min-width: 375px) {
+    .container {
+      font-size: 1em;
+      font-weight: normal;
+    }
+  }
+</style>
